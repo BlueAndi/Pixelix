9543849f991f98fc0f5a1136b5f945d2cba48dfd
Andreas Merkle <web@blue-andi.de>
2019-10-06 19:42:41 +0200

    Batch file added, which helps to analyze a backtrace. Just call it with the backtrace addresses as arguments.

---
+++ analyze_backtrace.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set ADDR2LINE=%userprofile%\.platformio\packages\toolchain-xtensa32\bin\xtensa-esp32-elf-addr2line.exe
+set FIRMWARE=.pio\build\esp32doit-devkit-v1\firmware.elf
+
+if "" == "%1" (
+    echo "Usage: %0 <addresses>"
+    goto end
+)
+
+%ADDR2LINE% -a -e %FIRMWARE% -p -f -C -a %*
+
+:end
