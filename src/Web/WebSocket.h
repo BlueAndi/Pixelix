82a96eb0c4f82ff3b0a0edf5da34da8049eea34c
Andreas Merkle <web@blue-andi.de>
2019-12-08 22:55:09 +0100

    Websocket refactoring of the event handling.

--- src/Web/WebSocket.h
+++ src/Web/WebSocket.h
@@ -106,13 +106,72 @@ private:
      * Websocket event handler.
      * 
      * @param[in] server    Websocket server
-     * @param[in] client    Weboscket client
+     * @param[in] client    Websocket client
      * @param[in] type      Websocket event type
      * @param[in] arg       Websocket argument
      * @param[in] data      Websocket data
      * @param[in] len       Websocket data length in bytes
      */
     static void onEvent(AsyncWebSocket* server, AsyncWebSocketClient* client, AwsEventType type, void* arg, uint8_t* data, size_t len);
+
+    /**
+     * Websocket connect event handler.
+     * 
+     * @param[in] server    Websocket server
+     * @param[in] client    Websocket client
+     * @param[in] request   Websocket request
+     */
+    void onConnect(AsyncWebSocket* server, AsyncWebSocketClient* client, AsyncWebServerRequest* request);
+
+    /**
+     * Websocket disconnect event handler.
+     * 
+     * @param[in] server    Websocket server
+     * @param[in] client    Websocket client
+     */
+    void onDisconnect(AsyncWebSocket* server, AsyncWebSocketClient* client);
+
+    /**
+     * Websocket pong event handler.
+     * 
+     * @param[in] server    Websocket server
+     * @param[in] client    Websocket client
+     * @param[in] data      Websocket data
+     * @param[in] len       Websocket data length in bytes
+     */
+    void onPong(AsyncWebSocket* server, AsyncWebSocketClient* client, uint8_t* data, size_t len);
+
+    /**
+     * Websocket error event handler.
+     * 
+     * @param[in] server        Websocket server
+     * @param[in] client        Websocket client
+     * @param[in] reasonCode    Error reason code
+     * @param[in] reasonStr     Error reason string
+     * @param[in] reasonStrLen  Error reason string length
+     */
+    void onError(AsyncWebSocket* server, AsyncWebSocketClient* client, uint16_t reasonCode, const char* reasonStr, size_t reasonStrLen);
+
+    /**
+     * Websocket data event handler.
+     * 
+     * @param[in] server    Websocket server
+     * @param[in] client    Websocket client
+     * @param[in] info      Websocket frame info
+     * @param[in] data      Websocket data
+     * @param[in] len       Websocket data length in bytes
+     */
+    void onData(AsyncWebSocket* server, AsyncWebSocketClient* client, AwsFrameInfo* info, const uint8_t* data, size_t len);
+
+    /**
+     * Handle a websocket message.
+     * 
+     * @param[in] server    Websocket server
+     * @param[in] client    Weboscket client
+     * @param[in] msg       Websocket message (not '\0' terminated)
+     * @param[in] msgLen    Websocket message length
+     */
+    void handleMsg(AsyncWebSocket* server, AsyncWebSocketClient* client, const char* msg, size_t msgLen);
 
     /**
      * Write single data byte to all clients.
