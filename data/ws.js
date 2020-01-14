3d299327a32af8241013ecafc21eb012712e8d7b
BlueAndi <web@blue-andi.de>
2020-01-14 23:35:58 +0100

    Now you can install/uninstall plugins via web interface too.

--- data/ws.js
+++ data/ws.js
@@ -7,9 +7,34 @@ pixelix = {
 pixelix.ws.Client = function(options) {
 
     this.socket     = null;
-    this.cmd        = "";
-    this.resolve    = null;
-    this.reject     = null;
+    this.cmdQueue   = [];
+    this.pendingCmd = null;
+
+    this._sendCmdFromQueue = function() {
+        var msg = "";
+
+        if (0 < this.cmdQueue.length) {
+            this.pendingCmd = this.cmdQueue.shift();
+
+            msg = this.pendingCmd.name;
+
+            if (null !== this.pendingCmd.par) {
+                msg += ";" + this.pendingCmd.par;
+            }
+
+            console.info("Websocket command: " + msg);
+            this.socket.send(msg);
+        }
+    };
+
+    this._sendCmd = function(cmd) {
+
+        this.cmdQueue.push(cmd);
+
+        if (null === this.pendingCmd) {
+            this._sendCmdFromQueue();
+        }
+    };
 };
 
 pixelix.ws.Client.prototype.connect = function(options) {
@@ -57,110 +82,123 @@ pixelix.ws.Client.prototype._onMessage = function(msg) {
     var data    = msg.split(";");
     var status  = data.shift();
     var rsp     = {};
+    var index   = 0;
 
-    if ("ACK" === status) {
-        if ("GETDISP" === this.cmd) {
+    if (null === this.pendingCmd) {
+        console.error("No pending command, but response received.");
+    } else if ("ACK" === status) {
+        if ("GETDISP" === this.pendingCmd.name) {
             rsp.slotId = data.shift();
             rsp.data = data;
-            this.resolve(rsp);
-        } else if ("INSTALL" === this.cmd) {
+            this.pendingCmd.resolve(rsp);
+        } else if ("INSTALL" === this.pendingCmd.name) {
             rsp.slotId = parseInt(data[0]);
-            this.resolve(rsp);
-        } else if ("PLUGINS" === this.cmd) {
-            rsp.plugins = data;
-            this.resolve(rsp);
-        } else if ("SLOTS" === this.cmd) {
+            this.pendingCmd.resolve(rsp);
+        } else if ("PLUGINS" === this.pendingCmd.name) {
+            rsp.plugins = [];
+            for(index = 0; index < data.length; ++index) {
+                rsp.plugins.push(data[index].substring(1, data[index].length - 1));
+            }
+            this.pendingCmd.resolve(rsp);
+        } else if ("SLOTS" === this.pendingCmd.name) {
             rsp.maxSlots = parseInt(data.shift());
-            rsp.slots = data;
-            this.resolve(rsp);
-        } else if ("UNINSTALL" === this.cmd) {
-            this.resolve(rsp);
+            rsp.slots = [];
+            for(index = 0; index < data.length; ++index) {
+                rsp.slots.push(data[index].substring(1, data[index].length - 1));
+            }
+            this.pendingCmd.resolve(rsp);
+        } else if ("UNINSTALL" === this.pendingCmd.name) {
+            this.pendingCmd.resolve(rsp);
         } else {
-            console.error("Unknown command: " + this.cmd);
-            this.reject();
+            console.error("Unknown command: " + this.pendingCmd.name);
+            this.pendingCmd.reject();
         }
     } else {
-        console.error("Command " + this.cmd + " failed.");
-        this.reject();
+        console.error("Command " + this.pendingCmd.name + " failed.");
+        this.pendingCmd.reject();
     }
 
-    this.cmd = "";
-    this.resolve = null;
-    this.reject = null;
+    this.pendingCmd = null;
+    this._sendCmdFromQueue();
 
     return;
 };
 
 pixelix.ws.Client.prototype.getDisplayContent = function() {
     return new Promise(function(resolve, reject) {
-
         if (null === this.socket) {
             reject();
         } else {
-            this.cmd = "GETDISP";
-            this.resolve = resolve;
-            this.reject = reject;
-            this.socket.send(this.cmd);
+            this._sendCmd({
+                name: "GETDISP",
+                par: null,
+                resolve: resolve,
+                reject: reject
+            });
         }
     }.bind(this));
 };
 
 pixelix.ws.Client.prototype.getSlots = function() {
     return new Promise(function(resolve, reject) {
-
         if (null === this.socket) {
             reject();
         } else {
-            this.cmd = "SLOTS";
-            this.resolve = resolve;
-            this.reject = reject;
-            this.socket.send(this.cmd);
+            this._sendCmd({
+                name: "SLOTS",
+                par: null,
+                resolve: resolve,
+                reject: reject
+            });
         }
     }.bind(this));
 };
 
 pixelix.ws.Client.prototype.getPlugins = function() {
     return new Promise(function(resolve, reject) {
-
         if (null === this.socket) {
             reject();
         } else {
-            this.cmd = "PLUGINS";
-            this.resolve = resolve;
-            this.reject = reject;
-            this.socket.send(this.cmd);
+            this._sendCmd({
+                name: "PLUGINS",
+                par: null,
+                resolve: resolve,
+                reject: reject
+            });
         }
     }.bind(this));
 };
 
 pixelix.ws.Client.prototype.install = function(options) {
     return new Promise(function(resolve, reject) {
-
         if (null === this.socket) {
             reject();
         } else if ("string" !== typeof options.pluginName) {
             reject();
         } else {
-            this.cmd = "INSTALL;" + options.pluginName;
-            this.resolve = resolve;
-            this.reject = reject;
-            this.socket.send(this.cmd);
+            this._sendCmd({
+                name: "INSTALL",
+                par: "\"" + options.pluginName + "\"",
+                resolve: resolve,
+                reject: reject
+            });
         }
     }.bind(this));
 };
 
 pixelix.ws.Client.prototype.uninstall = function(options) {
     return new Promise(function(resolve, reject) {
-
         if (null === this.socket) {
             reject();
         } else if ("number" !== typeof options.slotId) {
             reject();
         } else {
-            this.cmd = "UNINSTALL;" + options.slotId;
-            this.resolve = resolve;
-            this.reject = reject;
-            this.socket.send(this.cmd);
+            this._sendCmd({
+                name: "UNINSTALL",
+                par: options.slotId,
+                resolve: resolve,
+                reject: reject
+            });
         }
     }.bind(this));
 };
