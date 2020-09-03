bcb2e60b3901c7a2dea4b4a57beff620ac38e2bc
Andreas Merkle <web@blue-andi.de>
2020-09-03 16:50:50 +0200

    The CountdownPlugin web page reads now the configuration data from the selected plugin instance back and fills the form data.

--- data/js/utils.js
+++ data/js/utils.js
@@ -1,6 +1,5 @@
 var utils = {};
 
-/* Enable/Disable all form elements. */
 utils.enableForm = function(formId, enableIt) {
     var form        = document.getElementById(formId);
     var elements    = form.elements;
@@ -18,4 +17,45 @@ utils.injectOrigin = function(name, searchFor) {
     for(index = 0; index < elements.length; ++index) {
         elements[index].innerHTML = elements[index].innerHTML.replace(searchFor, location.origin);
     }
-}
+};
+
+utils.makeRequest = function(options) {
+    return new Promise(function(resolve, reject) {
+        if ("object" !== typeof options) {
+            reject({ msg: "Arguments are missing." });
+        } else if ("string" !== typeof options.method) {
+            reject({ msg: "Request method is missing." });
+        } else if ("string" !== typeof options.url) {
+            reject({ msg: "URL is missing." });
+        } else {
+            var xhr = new XMLHttpRequest();
+            var formData = null;
+
+            if ("object" === typeof options.parameter) {
+                formData = new FormData();
+
+                for( var key in options.parameter ) {
+                    formData.append(key, options.parameter[key]);
+                }
+            }
+
+            xhr.open(options.method, options.url);
+            xhr.onload = function() {
+                var jsonRsp = JSON.parse(xhr.response);
+        
+                if (0 === jsonRsp.status) {
+                    resolve(jsonRsp);
+                } else {
+                    reject(jsonRsp);
+                }
+            };
+
+            if (null === formData) {
+                xhr.send();
+            } else {
+                xhr.send(formData);
+            }
+        }
+    });
+};
+
