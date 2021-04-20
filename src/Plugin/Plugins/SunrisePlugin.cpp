30f16762c7b9843f25d47fc6098a384456e27258
BlueAndi <web@blue-andi.de>
2021-04-20 23:14:45 +0200

    Its not necessary to consider AM/PM, because the API is used non-formatted, which means there is no "AM/PM" available in the received string.

--- src/Plugin/Plugins/SunrisePlugin.cpp
+++ src/Plugin/Plugins/SunrisePlugin.cpp
@@ -346,6 +346,25 @@ void SunrisePlugin::initHttpClient()
         StaticJsonDocument<FILTER_SIZE> filter;
         DeserializationError            error;
 
+        /* Example:
+        * {
+        *   "results":
+        *   {
+        *     "sunrise":"2015-05-21T05:05:35+00:00",
+        *     "sunset":"2015-05-21T19:22:59+00:00",
+        *     "solar_noon":"2015-05-21T12:14:17+00:00",
+        *     "day_length":51444,
+        *     "civil_twilight_begin":"2015-05-21T04:36:17+00:00",
+        *     "civil_twilight_end":"2015-05-21T19:52:17+00:00",
+        *     "nautical_twilight_begin":"2015-05-21T04:00:13+00:00",
+        *     "nautical_twilight_end":"2015-05-21T20:28:21+00:00",
+        *     "astronomical_twilight_begin":"2015-05-21T03:20:49+00:00",
+        *     "astronomical_twilight_end":"2015-05-21T21:07:45+00:00"
+        *   },
+        *    "status":"OK"
+        * }
+        */
+
         filter["results"]["sunrise"]    = true;
         filter["results"]["sunset"]     = true;
 
@@ -396,11 +415,11 @@ String SunrisePlugin::addCurrentTimezoneValues(const String& dateTimeString) con
 {
     tm          gmTimeInfo;
     tm*         lcTimeInfo          = nullptr;
-    tm          cvTimeInfo;
     time_t      gmTime;
     char        timeBuffer[17]      = { 0 };
     const char* formattedTimeString = ClockDrv::getInstance().getTimeFormat() ? "%H:%M" : "%I:%M %p";
-    bool        isPM                = dateTimeString.endsWith("PM");
+
+    /* Example: "2015-05-21T05:05:35+00:00" */
 
     /* Convert date/time string to GMT time information */
     (void)strptime(dateTimeString.c_str(), "%Y-%m-%dT%H:%M:%S", &gmTimeInfo);
@@ -409,16 +428,8 @@ String SunrisePlugin::addCurrentTimezoneValues(const String& dateTimeString) con
     gmTime = mktime(&gmTimeInfo);
     lcTimeInfo = localtime(&gmTime);
 
-    /* Consider AM/PM */
-    cvTimeInfo = *lcTimeInfo;
-    
-    if (true == isPM)
-    {
-        cvTimeInfo.tm_hour += 12;
-    }
-
     /* Convert time information to user friendly string. */
-    (void)strftime(timeBuffer, sizeof(timeBuffer), formattedTimeString, &cvTimeInfo);
+    (void)strftime(timeBuffer, sizeof(timeBuffer), formattedTimeString, lcTimeInfo);
 
     return timeBuffer;
 }
