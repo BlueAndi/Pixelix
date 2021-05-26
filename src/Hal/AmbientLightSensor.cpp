230040a8f663e5c53aeffab64f0a2ddd9ad94923
BlueAndi <web@blue-andi.de>
2021-05-26 19:16:06 +0200

    Indention and typo fixed.

--- src/Hal/AmbientLightSensor.cpp
+++ src/Hal/AmbientLightSensor.cpp
@@ -135,69 +135,69 @@ float AmbientLightSensor::getIlluminance()
     if (NO_LDR_THRESHOLD < ADC_UINT16)
     {
         /* LDR GL5528
-        * from datasheet:
-        * gamma gradient = 0.7
-        * resistance at 10 Lux = 10 kOhm
-        *
-        * Calculation of R_LDR:
-        * x = log10( I )
-        * y = log10( R_LDR )
-        *
-        * Note, I is the ilumiance and R_LDR is the ambient light depended resistance.
-        *
-        * y = m * x + b
-        * log10( R_LDR ) = -(gamma gradient) * log10( I ) + b
-        * log10( R_LDR ) = log10( I ^ -(gamma gradient) ) + b
-        * log10( R_LDR ) = log10( I ^ -(gamma gradient) ) + log10( 10 ^ b )
-        * log10( R_LDR ) = log10( ( I ^ -(gamma gradient) ) * ( 10 ^ b ) )
-        * R_LDR = ( I ^ -(gamma gradient) ) * ( 10 ^ b )
-        *
-        * Calculation of the function axis section:
-        * b = log10( R_LDR * I ^ (gamma gradient) )
-        * b = log10 ( 10 kOhm * 10 Lux ^ 0.7 )
-        * b = log10 ( 10^4 * 10^0.7 ) kOhm
-        * b = 4 + 0.7
-        * b = 4.7 kOhm
-        *
-        * Calculation of I:
-        * R_LDR = ( I ^ -(gamma gradient) ) * ( 10 ^ b )
-        * I ^ -(gamma gradient) = R_LDR / ( 10 ^ b )
-        * 1 / ( I ^ (gamma gradient) ) = R_LDR / ( 10 ^ b )
-        * I ^ (gamma gradient) = ( 10 ^ b ) / R_LDR
-        * I = ( ( 10 ^ b ) / R_LDR ) ^ ( 1 / gamma gradient )
-        * I = [ ( 10 ^ b ) ^ ( 1 / gamma gradient ) ] / [ R_LDR ^ ( 1 / gamma gradient ) ]
-        * I = [ 10 ^ ( b / gamma gradient ) ] / R_LDR ^ ( 1 / gamma gradient )
-        * I = [ 10 ^ ( b / gamma gradient ) ] * R_LDR ^ ( -1 / gamma gradient )
-        * I = [ 10 ^ ( 4.7 / 0.7 ) ] * R_LDR ^ ( -1 / 0.7 )
-        * I = 5179474.6792312 * R_LDR ^ -1.42857142857143 Lux
-        */
+         * from datasheet:
+         * gamma gradient = 0.7
+         * resistance at 10 Lux = 10 kOhm
+         *
+         * Calculation of R_LDR:
+         * x = log10( I )
+         * y = log10( R_LDR )
+         *
+         * Note, I is the illumiance and R_LDR is the ambient light depended resistance.
+         *
+         * y = m * x + b
+         * log10( R_LDR ) = -(gamma gradient) * log10( I ) + b
+         * log10( R_LDR ) = log10( I ^ -(gamma gradient) ) + b
+         * log10( R_LDR ) = log10( I ^ -(gamma gradient) ) + log10( 10 ^ b )
+         * log10( R_LDR ) = log10( ( I ^ -(gamma gradient) ) * ( 10 ^ b ) )
+         * R_LDR = ( I ^ -(gamma gradient) ) * ( 10 ^ b )
+         *
+         * Calculation of the function axis section:
+         * b = log10( R_LDR * I ^ (gamma gradient) )
+         * b = log10 ( 10 kOhm * 10 Lux ^ 0.7 )
+         * b = log10 ( 10^4 * 10^0.7 ) kOhm
+         * b = 4 + 0.7
+         * b = 4.7 kOhm
+         *
+         * Calculation of I:
+         * R_LDR = ( I ^ -(gamma gradient) ) * ( 10 ^ b )
+         * I ^ -(gamma gradient) = R_LDR / ( 10 ^ b )
+         * 1 / ( I ^ (gamma gradient) ) = R_LDR / ( 10 ^ b )
+         * I ^ (gamma gradient) = ( 10 ^ b ) / R_LDR
+         * I = ( ( 10 ^ b ) / R_LDR ) ^ ( 1 / gamma gradient )
+         * I = [ ( 10 ^ b ) ^ ( 1 / gamma gradient ) ] / [ R_LDR ^ ( 1 / gamma gradient ) ]
+         * I = [ 10 ^ ( b / gamma gradient ) ] / R_LDR ^ ( 1 / gamma gradient )
+         * I = [ 10 ^ ( b / gamma gradient ) ] * R_LDR ^ ( -1 / gamma gradient )
+         * I = [ 10 ^ ( 4.7 / 0.7 ) ] * R_LDR ^ ( -1 / 0.7 )
+         * I = 5179474.6792312 * R_LDR ^ -1.42857142857143 Lux
+         */
         const float MULTIPLICATOR   = 5179474.6792312F;     /* 10 ^ ( b / gamma gradient ) */
         const float EXPONENT        = -1.42857142857143F;   /* -1 / gamma gradient */
 
         /* Calculation of R_LDR from the ADC value:
-        * The schematic contains a voltage divider with R = 1 kOhm connected to GND.
-        * The supply voltage Vcc is 3.3 V.
-        * The ADC resolution is 4096.
-        *
-        * I = Vcc / ( R_LDR + R )
-        *
-        * V_R = R * I
-        * V_R = R * ( Vcc / ( R_LDR + R ) )
-        * V_R = R * Vcc / ( R_LDR + R )
-        *
-        * ADC = ( ADC resolution - 1 ) * V_R / Vcc
-        * ADC = ( ADC resolution - 1 ) * R * Vcc / ( ( R_LDR + R ) * Vcc )
-        * ADC = ( ADC resolution - 1 ) * R / ( R_LDR + R )
-        * ADC * ( R_LDR + R ) = ( ADC resolution - 1 ) * R
-        * ADC * R_LDR + ADC * R = ( ADC resolution - 1 ) * R
-        * ADC * R_LDR = ( ADC resolution - 1 ) * R - ADC * R
-        * R_LDR = [ ( ADC resolution - 1 ) * R - ADC * R ] / ADC
-        * R_LDR = ( ADC_max * R - ADC * R ) / ADC
-        *
-        * Final calculation of I [Lux]:
-        * I = 5179474.6792312 * R_LDR ^ -1.42857142857143
-        * I = 5179474.6792312 * [ ( ADC_max * R - ADC * R ) / ADC ] ^ -1.42857142857143
-        */
+         * The schematic contains a voltage divider with R = 1 kOhm connected to GND.
+         * The supply voltage Vcc is 3.3 V.
+         * The ADC resolution is 4096.
+         *
+         * I = Vcc / ( R_LDR + R )
+         *
+         * V_R = R * I
+         * V_R = R * ( Vcc / ( R_LDR + R ) )
+         * V_R = R * Vcc / ( R_LDR + R )
+         *
+         * ADC = ( ADC resolution - 1 ) * V_R / Vcc
+         * ADC = ( ADC resolution - 1 ) * R * Vcc / ( ( R_LDR + R ) * Vcc )
+         * ADC = ( ADC resolution - 1 ) * R / ( R_LDR + R )
+         * ADC * ( R_LDR + R ) = ( ADC resolution - 1 ) * R
+         * ADC * R_LDR + ADC * R = ( ADC resolution - 1 ) * R
+         * ADC * R_LDR = ( ADC resolution - 1 ) * R - ADC * R
+         * R_LDR = [ ( ADC resolution - 1 ) * R - ADC * R ] / ADC
+         * R_LDR = ( ADC_max * R - ADC * R ) / ADC
+         *
+         * Final calculation of I [Lux]:
+         * I = 5179474.6792312 * R_LDR ^ -1.42857142857143
+         * I = 5179474.6792312 * [ ( ADC_max * R - ADC * R ) / ADC ] ^ -1.42857142857143
+         */
         const float ADC_MAX         = static_cast<float>(Board::adcResolution - 1U);
         const float R               = 1000.0F;  /* Resistor in the voltage divider, connected to GND. */
         const float ADC_FLOAT       = static_cast<float>(ADC_UINT16);
