fa06ff16ef322fe875bc495b05ec2a111f19f3cb
BlueAndi <web@blue-andi.de>
2024-08-21 14:50:26 +0200

    To save some program space, integrate only the sensors which are available or may be available.

--- lib/Sensors/src/Sensors.cpp
+++ lib/Sensors/src/Sensors.cpp
@@ -48,6 +48,71 @@
  * Macros
  *****************************************************************************/
 
+/**
+ * Macro is used to check whether a sensor is enabled or disabled.
+ */
+#define IS_ENABLED(_configSensorSwitch) ((_configSensorSwitch) != 0)
+
+/**
+ * Calculates the next sensor id from the last one.
+ */
+#define SENSOR_ID_NEXT(_id)             ((_id) + 1)
+
+/**
+ * Route the last sensor id through.
+ */
+#define SENSOR_ID_NOT_APPLICABLE(_id)   (_id)
+
+#if IS_ENABLED(CONFIG_SENSOR_LDR_ENABLE)
+/**
+ * LDR sensor id.
+ * Its 0 because its the first sensor in the gSensors list.
+ */
+#define SENSOR_ID_LDR       0
+#else   /* IS_ENABLED(CONFIG_SENSOR_LDR_ENABLE) */
+/**
+ * LDR sensor id.
+ * Its -1 because its the first sensor in the gSensors list, but not used.
+ */
+#define SENSOR_ID_LDR       -1
+#endif /* IS_ENABLED(CONFIG_SENSOR_LDR_ENABLE) */
+
+#if IS_ENABLED(CONFIG_SENSOR_SHT3X_ENABLE)
+/**
+ * SHT3x sensor id.
+ */
+#define SENSOR_ID_SHT3X     SENSOR_ID_NEXT(SENSOR_ID_LDR)
+#else   /* IS_ENABLED(CONFIG_SENSOR_SHT3X_ENABLE) */
+/**
+ * SHT3x sensor id (not applicable).
+ */
+#define SENSOR_ID_SHT3X     SENSOR_ID_NOT_APPLICABLE(SENSOR_ID_LDR)
+#endif  /* IS_ENABLED(CONFIG_SENSOR_SHT3X_ENABLE) */
+
+#if IS_ENABLED(CONFIG_SENSOR_DHT11_ENABLE)
+/**
+ * DHT11 sensor id.
+ */
+#define SENSOR_ID_DHT11     SENSOR_ID_NEXT(SENSOR_ID_SHT3X)
+#else   /* IS_ENABLED(CONFIG_SENSOR_DHT11_ENABLE) */
+/**
+ * DHT11 sensor id (not applicable).
+ */
+#define SENSOR_ID_DHT11     SENSOR_ID_NOT_APPLICABLE(SENSOR_ID_SHT3X)
+#endif  /* IS_ENABLED(CONFIG_SENSOR_DHT11_ENABLE) */
+
+#if IS_ENABLED(CONFIG_SENSOR_BATTERY_ENABLE)
+/**
+ * Battery sensor id.
+ */
+#define SENSOR_ID_BATTERY   SENSOR_ID_NEXT(SENSOR_ID_DHT11)
+#else   /* IS_ENABLED(CONFIG_SENSOR_BATTERY_ENABLE) */
+/**
+ * Battery sensor id (not applicable).
+ */
+#define SENSOR_ID_BATTERY   SENSOR_ID_NOT_APPLICABLE(SENSOR_ID_DHT11)
+#endif  /* IS_ENABLED(CONFIG_SENSOR_BATTERY_ENABLE) */
+
 /******************************************************************************
  * Types and classes
  *****************************************************************************/
@@ -60,30 +125,57 @@
  * Local Variables
  *****************************************************************************/
 
+#if IS_ENABLED(CONFIG_SENSOR_LDR_ENABLE)
+
 /** The LDR is used for automatic display brightness control. */
 static SensorLdr        gLdr(CONFIG_SENSOR_LDR, CONFIG_SENSOR_LDR_SERIES_RESISTANCE);
+
+#endif /* IS_ENABLED(CONFIG_SENSOR_LDR_ENABLE) */
+
+#if IS_ENABLED(CONFIG_SENSOR_SHT3X_ENABLE)
 
 /** The SHT3x sensor in autodetect mode (for two-wire sensors only). */
 static SensorSht3X      gSht3x(SHTSensor::AUTO_DETECT);
 
+#endif /* IS_ENABLED(CONFIG_SENSOR_SHT3X_ENABLE) */
+
+#if IS_ENABLED(CONFIG_SENSOR_DHT11_ENABLE)
+
 /** The DHT11 sensor. */
 static SensorDhtX       gDht11(SensorDhtX::MODEL_DHT11);
+
+#endif /* IS_ENABLED(CONFIG_SENSOR_DHT11_ENABLE) */
+
+#if IS_ENABLED(CONFIG_SENSOR_BATTERY_ENABLE)
 
 /** Battery sensor. */
 static SensorBattery    gBattery;
 
+#endif /* IS_ENABLED(CONFIG_SENSOR_BATTERY_ENABLE) */
+
 /** A list with all registered sensors. */
 static ISensor*         gSensors[] =
 {
-    /* Sensor id, Sensor driver */
-    /* 0 */ &gLdr,
-    /* 1 */ &gSht3x,
-    /* 2 */ &gDht11,
-    /* 3 */ &gBattery
+#if IS_ENABLED(CONFIG_SENSOR_LDR_ENABLE)
+    &gLdr,
+#endif /* IS_ENABLED(CONFIG_SENSOR_LDR_ENABLE) */
+
+#if IS_ENABLED(CONFIG_SENSOR_SHT3X_ENABLE)
+    &gSht3x,
+#endif /* IS_ENABLED(CONFIG_SENSOR_SHT3X_ENABLE) */
+
+#if IS_ENABLED(CONFIG_SENSOR_DHT11_ENABLE)
+    &gDht11,
+#endif /* IS_ENABLED(CONFIG_SENSOR_DHT11_ENABLE) */
+
+#if IS_ENABLED(CONFIG_SENSOR_BATTERY_ENABLE)
+    &gBattery,
+#endif /* IS_ENABLED(CONFIG_SENSOR_BATTERY_ENABLE) */
+    nullptr
 };
 
 /** The concrete sensor data provider implementation. */
-static SensorDataProviderImpl   gSensorDataProviderImpl(gSensors, UTIL_ARRAY_NUM(gSensors));
+static SensorDataProviderImpl   gSensorDataProviderImpl(gSensors, UTIL_ARRAY_NUM(gSensors) - 1U);
 
 /**
  * Default offset table, used to initialize the sensor channel offset once in the very
@@ -91,8 +183,10 @@ static SensorDataProviderImpl   gSensorDataProviderImpl(gSensors, UTIL_ARRAY_NUM
  */
 static const SensorChannelDefaultValue gSensorDefaultValues[] =
 {
-    /* Sensor id    Channel id      Value as JSON string */
-    {   1U,         0U,             "{ offset: -9 }"  }    /* SHT3x temperature offset */
+    /*  Sensor id           Channel id  Value as JSON string */
+#if IS_ENABLED(CONFIG_SENSOR_SHT3X_ENABLE)
+    {   SENSOR_ID_SHT3X,    0U,         "{ offset: -9 }"  }    /* SHT3x temperature offset */
+#endif /* IS_ENABLED(CONFIG_SENSOR_SHT3X_ENABLE) */
 };
 
 /******************************************************************************
